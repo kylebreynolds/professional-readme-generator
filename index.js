784cb33d8342a2abae6ae40ab6c1b9fc4090b842
Kyle Reynolds <kbreyn06@gmail.com>
2022-05-22 14:34:13 -0500

    prompt questions, generate readme

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { Z_ERRNO } = require('zlib');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+        type:'input',
+        name:'title',
+        message:'What is the title of this application?'
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'What is the purpose of your application?'
+    },
+    {
+        type:'input',
+        name:'instructions',
+        message:'How do you install your application?'
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'How will your application be used?'
+    },
+    {
+        type:'input',
+        name:'contributions',
+        message:'Who contributed to this application?'
+    }
+];
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(userInput) {
+
+        const readMe = generateMarkdown(userInput)
+
+        // TODO: Create a function to write README file
+        fs.writeFile('README.md', readMe, err => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log('README COMPLETE!')
+            }
+        });
+
+    })
+}
 
 // Function call to initialize app
 init();
